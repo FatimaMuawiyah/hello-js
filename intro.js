f4b5cf3855e54a867643d90716844a6a4da91433
Fatima Muawiyah Halilu <Muawiyahfatima58@gmail.com>
2026-02-12 17:55:18 +0300

    Initial commit:  My_personal_Info

---
+++ intro.js
@@ -0,0 +1,8 @@
+const user = {
+    name: "Fatima Muawiyah Halilu",
+    age: 23,
+    favourite_programming_langauge: "Java"
+};
+
+console.log(user.name);
+console.log("My Favourite Programming language is" + " " + user.favourite_programming_langauge);
